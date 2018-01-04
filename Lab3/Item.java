2e6215186a1be7e0c2295058e16280cb53a5b5ff
jessicagerson <jessygerson123@gmail.com>
2018-01-04 12:34:05 -0800

    ch6lab3DONE

---
+++ Lab3/Item.java
@@ -0,0 +1,63 @@
+package Lab3;
+
+
+// ***************************************************************
+
+//   Item.java
+
+//
+
+//   Represents an item in a shopping cart.
+
+// ***************************************************************
+
+
+public class Item
+
+{
+
+    public String name;
+
+    public double price;
+
+    public int quantity;
+
+
+    // -------------------------------------------------------
+
+    //  Create a new item with the given attributes.
+
+    // -------------------------------------------------------
+
+    public Item (String itemName, double itemPrice, int numPurchased)
+
+    {
+
+        name = itemName;
+
+        price = itemPrice;
+
+        quantity = numPurchased;
+
+    }
+
+
+    // -------------------------------------------------------
+
+    //   Return a string with the information about the item
+
+    // -------------------------------------------------------
+
+    public String toString ()
+
+    {
+
+        //Your code goes here
+        return "You are buying " + this.quantity+ " " + this.name + "s that cost " + this.price + " dollars.";
+    }
+
+
+    } 
+
+
+
